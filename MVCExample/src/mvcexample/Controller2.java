263470f5d1f82521763ba406966f110fc9730aa6
ryannbarnett <ryb00030@students.stir.ac.uk>
2024-03-04 09:28:24 +0000

    Task 4
    Added button to controller 2 class to increment database B, model notifies observers

--- MVCExample/src/mvcexample/Controller2.java
+++ MVCExample/src/mvcexample/Controller2.java
@@ -17,6 +17,7 @@ public class Controller2 extends JFrame
     private View3 view3;
     private View4 view4;
     private JButton clearViews;   // For direct message to views
+    private JButton incB;
     //private JButton refreshViews; // To prompt them to refresh their contents from the model
  
     // Constructor
@@ -37,6 +38,9 @@ public class Controller2 extends JFrame
         clearViews = new JButton("Clear views");
         window.add(clearViews);
         clearViews.addActionListener(this);
+        incB = new JButton("Increment B");
+        window.add(incB);
+        incB.addActionListener(this);
         /*
         // [simon] this is no longer needed (see part 1)
         refreshViews = new JButton("Refresh views");
@@ -60,6 +64,8 @@ public class Controller2 extends JFrame
         if (e.getSource() == clearViews) {
             view3.clear();
             view4.clear();
+        } else if (e.getSource() == incB) {
+            model.modifyB();
         }
         /*
         // [simon] this is no longer needed (see part 1)
--- MVCExample/src/mvcexample/Model.java
+++ MVCExample/src/mvcexample/Model.java
@@ -18,6 +18,13 @@ public class Model extends Observable {
         notifyObservers();         // Note: notifies ALL Observing views
         
     } // modifyA
+
+    public void modifyB() {
+
+        dataBaseB++;
+        setChanged();
+        notifyObservers();
+    }
   
     public int getDataA() {
         
